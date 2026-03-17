2fdcac0e7c640b41e842d33d80a429e7c1b3010a
Saddam Solnkey <saddam@MacBookPro.corp.softvolt.in>
2026-03-17 12:34:37 +0530

    issue fix

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -89,6 +89,7 @@ class ProductController extends Controller
     
     public function getallproduct(Request $request)
     {
+        return;
         if ((int)($request->buildnumber ?? 0) < 5) {
             return response(['error' => "Error", 'message' => 'Update the App']);
         }
@@ -126,7 +127,7 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {   
-        
+        return;
         $data = $request->all();
         $validator = Validator::make($data, [
             'mobile_name' => 'required|max:255',
